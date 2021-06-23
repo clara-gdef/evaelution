1ec210b7aeceacd9f69dcd1cfe7812716c26db82
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2021-06-23 15:40:14 +0200

    wip

--- data/visualisation/tsne_in_vae_space.py
+++ data/visualisation/tsne_in_vae_space.py
@@ -10,6 +10,7 @@ from matplotlib.pyplot import cm
 from models.train_vae import make_xp_title
 from models.classes import VAE
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 import numpy as np
 from utils.models import get_latest_model, index_to_one_hot
 import matplotlib.lines as mlines
@@ -25,13 +26,13 @@ def init(args):
             model_name = "/".join(xp_title.split('_'))
             model_path = os.path.join(CFG['modeldir'], model_name)
             model, epoch = load_model(xp_title, model_path, model_name)
-            return main(args, model.cuda(), model_name, epoch)
+            return main(args, model.cuda(), xp_title, epoch)
     else:
         xp_title = make_xp_title(args)
         model_name = "/".join(xp_title.split('_'))
         model_path = os.path.join(CFG['modeldir'], model_name)
         model, epoch = load_model(xp_title, model_path, model_name)
-        return main(args, model.cuda(), model_name, epoch)
+        return main(args, model.cuda(), xp_title, epoch)
 
 
 def main(args, model, model_name, epoch):
@@ -41,7 +42,7 @@ def main(args, model, model_name, epoch):
     test_projections = project_points(sub_test, model, "test")
     trans_points_train, ind_labels_train, exp_labels_train = prep_data_for_viz(train_projections, "train")
     trans_points_test, ind_labels_test, exp_labels_test = prep_data_for_viz(test_projections, "test")
-    plot_tsne(trans_points_train, ind_labels_train, exp_labels_train,
+    plot_proj(trans_points_train, ind_labels_train, exp_labels_train,
               trans_points_test, ind_labels_test, exp_labels_test,
               model_name, epoch)
     ipdb.set_trace()
@@ -93,37 +94,50 @@ def prep_data_for_viz(data_dict, split):
     points = [i["point"] for i in data_dict]
     ind_labels = [i["ind_index"] for i in data_dict]
     exp_labels = [i["exp_index"] for i in data_dict]
-    trans_points = fit_transform_by_tsne(points, split)
+    if args.proj_type == "tsne":
+        trans_points = fit_transform_by_tsne(points, split)
+    elif args.proj_type == "pca":
+        trans_points = fit_transform_by_pca(points, split)
+    else:
+        ipdb.set_trace()
+        raise Exception()
     return trans_points, ind_labels, exp_labels
 
 
 def fit_transform_by_tsne(input_data, split):
     print(f"Fitting TSNE on split {split} for {args.n_tsne} components, {len(input_data)} samples...")
-    data_embedded = TSNE(n_components=args.n_tsne, n_jobs=-1, verbose=1).fit_transform(np.array(input_data).squeeze(1))
+    data_embedded = TSNE(n_components=args.n_comp, n_jobs=-1, verbose=1).fit_transform(np.array(input_data).squeeze(1))
     print(f"TSNE fitted!")
     return data_embedded
 
 
-def plot_tsne(points_train, inds_train, exps_train, points_test, inds_test, exps_test, model_name, epoch):
+def fit_transform_by_pca(input_data, split):
+    print(f"Fitting PCA on split {split} for {args.n_tsne} components, {len(input_data)} samples...")
+    data_embedded = PCA(n_components=args.n_comp).fit_transform(np.array(input_data).squeeze(1))
+    print(f"PCA fitted!")
+    return data_embedded
+
+
+def plot_proj(points_train, inds_train, exps_train, points_test, inds_test, exps_test, model_name, epoch):
     NUM_COLORS = 20
     shape_per_exp, color_legends = get_dicts_for_plot()
     color = cm.rainbow(np.linspace(0, 1, NUM_COLORS))
 
     fig = plt.figure(figsize=(15, 8))
-    fig.suptitle(f"TSNE projection of VAE space", fontsize=14)
+    fig.suptitle(f"{args.proj_type.upper()} projection of VAE space", fontsize=14)
 
     ax = fig.add_subplot(211)
     for num, i in enumerate(range(len(points_train))):
         ax.scatter(points_train[num, 0], points_train[num, 1],
                    color=color[inds_train[num]], cmap=color, marker=shape_per_exp[exps_train[num]])
-        ax.set_title(f"TSNE of {len(points_train)} jobs of split train")
+        ax.set_title(f"{args.proj_type.upper()} of {len(points_train)} jobs of split train")
     ax.axis('tight')
 
     ax = fig.add_subplot(212)
     for num, i in enumerate(range(len(points_test))):
         ax.scatter(points_test[num, 0], points_test[num, 1],
                    color=color[inds_test[num]], cmap=color, marker=shape_per_exp[exps_test[num]])
-        ax.set_title(f"TSNE of {len(points_test)} jobs of split test")
+        ax.set_title(f"{args.proj_type.upper()} of {len(points_test)} jobs of split test")
     ax.axis('tight')
 
     handles = []
@@ -139,7 +153,7 @@ def plot_tsne(points_train, inds_train, exps_train, points_test, inds_test, exps
     fig.legend(handles=handles)
     ipdb.set_trace()
     plt.show()
-    plt.savefig(f'tsne_{model_name}_ep{epoch}.png')
+    plt.savefig(f'{args.proj_type}_{model_name}_ep{epoch}.png')
 
 
 def get_dicts_for_plot():
@@ -149,7 +163,6 @@ def get_dicts_for_plot():
     ind_file = "20_industry_dict.pkl"
     with open(os.path.join(CFG["gpudatadir"], ind_file), 'rb') as f_name:
         industry_dict = pkl.load(f_name)
-    ipdb.set_trace()
     color_legends = {k: v for k, v in industry_dict.items()}
     return shape_per_exp, color_legends
 
@@ -160,7 +173,8 @@ if __name__ == "__main__":
     parser.add_argument("--subsample", type=int, default=-1)
     parser.add_argument("--exp_type", type=str, default="uniform")
     parser.add_argument("--num_point_per_group", type=int, default=5)
-    parser.add_argument("--n_tsne", type=int, default=2)
+    parser.add_argument("--n_comp", type=int, default=2)
+    parser.add_argument("--proj_type", type=str, default="pca") # pca or tsne
     # model attributes
     parser.add_argument("--freeze_decoding", type=str, default="True")
     parser.add_argument("--b_size", type=int, default=128)
