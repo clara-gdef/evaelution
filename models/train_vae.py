ae0bdf28f41ab8e321f9f0de94f5892e54f62980
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2021-06-28 11:31:33 +0200

    replace std by log var

--- models/train_vae.py
+++ models/train_vae.py
@@ -208,7 +208,7 @@ if __name__ == "__main__":
     parser.add_argument("--test_b_size", type=int, default=1)
     parser.add_argument("--clip_val", type=float, default=.5)
     parser.add_argument("--plot_latent_space", type=str, default="True")
-    parser.add_argument("--plot_grad", type=str, default="True")
+    parser.add_argument("--plot_grad", type=str, default="False")
     parser.add_argument("--proj_type", type=str, default="tsne")
     parser.add_argument("--n_comp", type=int, default=2)
     parser.add_argument("--att_type", type=str, default="both") # can be both, exp or ind
