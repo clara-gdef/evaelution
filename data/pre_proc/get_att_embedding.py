b91ac2facaac618fc815e982cdc7217f1c54a623
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2021-06-29 17:44:09 +0200

    wip

--- data/pre_proc/get_att_embedding.py
+++ data/pre_proc/get_att_embedding.py
@@ -36,13 +36,35 @@ def main(args):
     dict_fr = {num: name for num, name in enumerate(ind_fr)}
     with open(os.path.join(CFG["gpudatadir"], tgt_ind_file), 'wb') as f_name:
         pkl.dump(dict_fr, f_name)
+
+    exp_file = "exp_dict_fr_3.pkl"
+    with open(os.path.join(CFG["gpudatadir"], exp_file), 'rb') as f_name:
+        exps = pkl.load(f_name)
     ipdb.set_trace()
 
     tokenizer = CamembertTokenizer.from_pretrained("camembert-base")
     text_encoder = CamembertModel.from_pretrained('camembert-base')
+    emb_dim = text_encoder.embeddings.word_embeddings.embedding_dim
 
-    ind_embeddings = torch.zeros(20, )
+    ind_embeddings = torch.zeros(20, emb_dim)
+    for num, name in dict_fr.items():
+        name_tok = tokenizer(name, return_tensors="pt")
+        emb = text_encoder(name_tok["input_ids"])
+        ind_embeddings[num, :] = emb
 
+    tgt_ind_file = "ind_20_embeddings_fr.pkl"
+    with open(os.path.join(CFG["gpudatadir"], tgt_ind_file), 'wb') as f_name:
+        pkl.dump(ind_embeddings, f_name)
+
+    exp_embeddings = torch.zeros(3, emb_dim)
+    for num, name in exps.items():
+        name_tok = tokenizer(name, return_tensors="pt")
+        emb = text_encoder(name_tok["input_ids"])
+        exp_embeddings[num, :] = emb
+
+    tgt_exp_file = "exp_3_embeddings_fr.pkl"
+    with open(os.path.join(CFG["gpudatadir"], tgt_exp_file), 'wb') as f_name:
+        pkl.dump(exp_embeddings, f_name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
